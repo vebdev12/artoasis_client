15a0b148615747a7b7e3424cff1cd806d539e6dc
Богдан Беседин <bogdanbesedin@MBP-Bogdan.IGD_Rostelecom>
2025-06-11 22:46:03 +0300

    change url

--- scripts/request.js
+++ scripts/request.js
@@ -1,5 +1,5 @@
-// const BASE_URL = "https://artoasis-server.onrender.com/api";
-const BASE_URL = "http://localhost:8080/api";
+const BASE_URL = "https://artoasis-server.onrender.com/api";
+// const BASE_URL = "http://localhost:8080/api";
 
 const defaultHeaders = {
   "Content-type": "application/json",
--- scripts/setFile.js
+++ scripts/setFile.js
@@ -1,6 +1,6 @@
 const setFile = (url) => {
   if (!url) return;
 
-  // return `https://artoasis-server.onrender.com/${url}`;
-  return `http://localhost:8080/${url}`;
+  return `https://artoasis-server.onrender.com/${url}`;
+  // return `http://localhost:8080/${url}`;
 };
